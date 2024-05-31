f4ccb33c1d98afde998f74623055716c5da9d6b1
Yura Sushko <yurasushko2000@gmail.com>
2024-05-31 18:25:37 +0300

    Changes by shaib

--- src/Constans.java
+++ src/Constans.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 public class Constans {
     public static final int WIDTH=800;
     public static final int HIGHT=600;
@@ -17,6 +19,24 @@ public class Constans {
     public static final int SLING_SHOT_WIDTH=50;
     public static final int SLING_SHOT_ARM_LENGTH=70;
     public static final int SLING_SHOT_LOCATION_X=100;
-}
+    public static final String PLAY_BUTTON_TEXT ="play";
+    public static final String INSTRUCTIONS_BUTTON_TEXT="instructions";
+    public static final String EXIT_BUTTON_TEXT="exit";
+
+    public static final int OPENINIG_SCREEN_PLAY_BUTTON_X=250;
+    public static final int OPENINIG_SCREEN_PLAY_BUTTON_Y=150;
+    public static final int OPENINIG_SCREEN_INSTRUCTIONS_BUTTON_X=250;
+    public static final int OPENINIG_SCREEN_INSTRUCTIONS_BUTTON_Y=300;
+    public static final int OPENINIG_SCREEN_BOTH_BUTTON_WIDTH=300;
+    public static final int OPENINIG_SCREEN_BOTH_BUTTON_HIGHT=100;
+
+    public static final int INSTRUCTIONS_SCREEN_EXIT_BUTTON_X=0;
+    public static final int INSTRUCTIONS_SCREEN_EXIT_BUTTON_Y=0;
+    public static final int INSTRUCTIONS_SCREEN_EXIT_BUTTON_WIDTH=100;
+    public static final int INSTRUCTIONS_SCREEN_EXIT_BUTTON_HIGHT=100;
+    public static final Color EXIT_BUTTON_COLOR=Color.gray;
+    public static final Color PLAY_BUTTON_COLOR=Color.red;
+    public static final Color INSTRUCTIONS_BUTTON_COLOR=Color.red;
+    }
 
 
